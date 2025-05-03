88c54197256ce9196dce91d494fed7349e5e4b5c
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-05-03 18:34:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="flitton-fib-rs",
+    name="fib-max-rs",
     version="0.1",
     rust_extensions=[RustExtension(
-        ".flitton_fib_rs.flitton_fib_rs",
+        ".fib_max_rs.fib_max_rs",
         path="Cargo.toml", binding=Binding.PyO3)],
-    packages=["flitton_fib_rs"],
+    packages=["fib_max_rs"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
