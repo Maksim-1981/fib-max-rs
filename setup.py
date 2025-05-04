ac0b454d98dd8e614cb084d3f5fe8ff1038d0dfc
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-05-04 14:39:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ dist.Distribution().fetch_build_eggs(['setuptools_rust'])
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+
 setup(
     name="fib-max-rs",
     version="0.1",
@@ -12,13 +13,28 @@ setup(
         path="Cargo.toml", binding=Binding.PyO3)],
     packages=["fib_max_rs"],
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Rust",
-        "Operating System :: POSIX",
-        "Operating System :: MacOS :: MacOS X",
-    ],
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Programming Language :: Rust",
+            "Operating System :: POSIX",
+            "Operating System :: MacOS :: MacOS X",
+        ],
+    # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'fib-number = fib_max_rs'
+            'fib_number_command:'
+            'fib_number_command',
+            'config-fib = fib_max_rs'
+            'config_number_command:'
+            'config_number_command',
+        ],
+    },
+    # requirements=[
+    #     "pyyaml>=3.13",
+    #     "numpy"
+    # ]
 )
