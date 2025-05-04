3746aebdd880571e4b36241444b6f3eddfc1c909
Maksim-1981 <97602604+Maksim-1981@users.noreply.github.com>
2025-05-04 11:16:45 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+
+
+#[pymodule]
+fn fib_max_rs(_py: Python, m: &PyModule) -> \
+PyResult<()> {
+m.add_wrapped(wrap_pyfunction!(say_hello));
+Ok(())
+}
